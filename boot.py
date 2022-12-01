07aa6fb2d9ab68cc744b4ee8f1ef7159017d0b16
frarge <93608464+frarge@users.noreply.github.com>
2022-12-01 11:42:12 +0100

    main updated

--- boot.py
+++ boot.py
@@ -5,12 +5,14 @@ import webrepl
 import network
 import senko
 import machine
+import gc
 
-GITHUB_URL = "https://github.com/frarge/esp32project/"
-OTA = senko.Senko(url=GITHUB_URL, files=["boot.py", "main.py"])
+
+
 
 wlan = network.WLAN(network.STA_IF)
 wlan.active(True)
+
 if not wlan.isconnected():
     print('connecting to network...')
     wlan.connect('TIM-19910111', 'Semipaghitelado.95')
@@ -18,8 +20,15 @@ if not wlan.isconnected():
         pass
     print('network config:', wlan.ifconfig())
 
+        
+gc.collect()
+gc.enable()
+        
+OTA = senko.Senko(user="frarge", repo="esp32project", files = ["main.py"])
 if OTA.update():
     print("Updated to the latest version! Rebooting...")
     machine.reset()
-
+       
+    
 webrepl.start(password = "frarge95")
+
--- main.py
+++ main.py
@@ -2,5 +2,5 @@ from machine import Pin
 from time import sleep
 led = Pin(2, Pin.OUT)
 while True:
-  led.value(not led.value())
-  sleep(0.5)
+  print("Hello World")
+  sleep(2000)
